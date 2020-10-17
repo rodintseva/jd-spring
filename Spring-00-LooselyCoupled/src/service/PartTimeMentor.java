ace5edd042a68f5b443c9781152def124e4264fe
Ekaterina Rodintseva <rodintseva@gmail.com>
2020-10-17 11:39:38 -0400

    loose Coup

---
+++ Spring-00-LooselyCoupled/src/service/PartTimeMentor.java
@@ -0,0 +1,12 @@
+package service;
+
+import implementation.Mentor;
+
+public class PartTimeMentor implements Mentor {
+
+        @Override
+        public void createAccount(){
+            System.out.println("Part Time Mentor Account is created.........");
+        }
+
+}
